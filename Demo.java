02f721efbb80553e47c3a4c2e4e149830f452eae
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-09-19 14:31:38 +0530

    sum of n

--- Demo.java
+++ Demo.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class Demo {
  public static void main(String args[]){
 
-   new Scanner(System.in);
+ Scanner sc = new Scanner(System.in);
 
-    int n = sc.nextInt;
-
- }
+    int n = sc.nextInt();
+    int i = 1;
+    int sum = 0;
     
+    while (i <= n) {
+ 
+      sum = sum+i;
+      i++;    
+    }
+    System.out.println(sum);
+ }
+
 }
