a99d186730c6ab73b707c7907033d09bb536c46d
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-12-09 15:48:00 +0530

    floid trangle

--- pattterns.java
+++ pattterns.java
@@ -1,19 +1,28 @@
 public class pattterns {
 
 
+public static void inverted_pyaramid_number(int n){
+    for(int i= 1; i <=n; i++){
+        for (int j = 1; j <= n-i+1; j++){
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+}
+
+
 public static void inverted_pyaramid(int n){
 
     for (int i = 1; i<= n; i++){
 
         for (int j = 1; j <= n-i; j++){
-
-            System.out.print(" ");
+             System.out.print(" ");
         }  
         for (int j = 1; j <= i ; j++){
-
-            System.out.print("*");
+             System.out.print("*");
         }
-        System.out.println();
+             System.out.println();
     }
 }
 
@@ -32,8 +41,22 @@ public static void inverted_pyaramid(int n){
     }
   }
 
+  public static void flyd_triangle(int n){
+    int ctr= 1;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i;j++){
+            System.out.print(ctr);
+            ctr++;
+        }
+        System.out.println();
+    }
+
+  }
+
     public static void main(String[] args) {
         // hollow_rectangle(4, 5);
-        inverted_pyaramid(4);
+        // inverted_pyaramid(4);
+        // inverted_pyaramid_number(5);
+        flyd_triangle(3);
     }
 }
